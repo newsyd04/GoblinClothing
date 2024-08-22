c194da826bfb38b3149a2c22b2c6d757002b62cf
newsyd04 <newsyd04@gmail.com>
2024-08-22 01:01:44 +0100

    adding config for https

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Replace with your Elastic Beanstalk URL
-const API_BASE_URL = 'http://GoblinClothing-backend-dev.eu-west-1.elasticbeanstalk.com/api';
+const API_BASE_URL = 'https://GoblinClothing-backend-dev.eu-west-1.elasticbeanstalk.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
