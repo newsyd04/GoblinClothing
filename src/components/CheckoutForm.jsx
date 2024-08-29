c0b5ae2bd9f956bce4d6058fd953a1d87069b290
newsyd04 <newsyd04@gmail.com>
2024-08-29 18:03:09 +0100

    Fixed Stripe unauthorized error

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -3,29 +3,35 @@ import React from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
 
-const stripePromise = loadStripe('your-publishable-key-here'); // Replace with your actual Stripe publishable key
+const stripePromise = loadStripe('pk_test_51PqYFwCbsRzgWQ8jPHOsRoIrkCXhUb1aiSAIT4GZZVOxj8IRmx9o7v3svcH1nSMSuJY7tJkiX4ttXBD5aHtkvzKN00Krn2Xi92'); // Replace with your actual Stripe publishable key
 
 function CheckoutForm({ cart, total }) {
   const navigate = useNavigate(); // Initialize the useNavigate hook
 
   const handleCheckout = async () => {
     const stripe = await stripePromise;
-
+  
     const response = await fetch('https://api.goblinclothing.com/api/products/create-checkout-session', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ cart }),
     });
-
+  
+    console.log('Response status:', response.status);
+  
+    // Read the response body as JSON
     const session = await response.json();
-
+  
+    // Log the JSON response
+    console.log('Session data:', session);
+  
     if (session.error) {
       console.error('[Checkout Session Error]', session.error);
     } else {
       const result = await stripe.redirectToCheckout({
         sessionId: session.id,
       });
-
+  
       if (result.error) {
         console.error('[Stripe Checkout Error]', result.error.message);
       }
