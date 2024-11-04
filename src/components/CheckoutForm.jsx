01b971ec38cba6607e15627407262b15e6bef059
conorglynn02 <conorglynn02@gmail.com>
2024-11-04 18:57:34 +0000

    Toast instead of alert

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,12 +1,15 @@
 // CheckoutForm.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
+import Toast from './Toast';
 
 const stripePromise = loadStripe('pk_test_51PqYFwCbsRzgWQ8jPHOsRoIrkCXhUb1aiSAIT4GZZVOxj8IRmx9o7v3svcH1nSMSuJY7tJkiX4ttXBD5aHtkvzKN00Krn2Xi92'); // Replace with your actual Stripe publishable key
 
 function CheckoutForm({ cart, total, email, name, address1, address2, city, county, eircode, country }) {
   const navigate = useNavigate(); // Initialize the useNavigate hook
+  const [showToast, setShowToast] = useState(false);
+  const [toastMessage, setToastMessage] = useState('');
 
   const checkoutClicked = () => {
     let passed = checkCheckout();
@@ -18,7 +21,8 @@ function CheckoutForm({ cart, total, email, name, address1, address2, city, coun
 
   const checkCheckout = () => {
     if (!email || !name || !address1 || !city || !county || !eircode || !country) {
-      alert('Please fill out all required fields.');
+      setToastMessage('Please fill out all required fields.');
+      setShowToast(true);
       return false;
     }
     return true;
@@ -55,12 +59,16 @@ function CheckoutForm({ cart, total, email, name, address1, address2, city, coun
   };
 
   return (
-    <button
-      onClick={checkoutClicked}
-      className="mt-4 bg-green-600 text-white py-2 px-4 rounded-md"
-    >
-      Pay {total} Shnargles
-    </button>
+    <>
+      <Toast message={toastMessage} show={showToast} onClose={() => setShowToast(false)} />
+
+      <button
+        onClick={checkoutClicked}
+        className="mt-4 bg-green-600 text-white py-2 px-4 rounded-md"
+      >
+        Pay {total} Shnargles
+      </button>
+    </>
   );
 }
 
